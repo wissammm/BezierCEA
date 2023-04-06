57c1b2ff8a913dc2c706e64e915c306075f49e9e
Boussella Wissam <wissamboussella@gmail.com>
2023-04-06 16:36:44 +0200

    Surement pas le temps de tester, tangent et normal, ne marche surrement pas Derivative incertaint, à voir demain

--- main.cpp
+++ main.cpp
@@ -34,7 +34,8 @@ struct Point
     double y;
 };
 
-struct Segment{
+struct Segment
+{
     Point a;
     Point b;
 };
@@ -52,7 +53,7 @@ Point subPoint(const Point p1, const Point p2, int factor)
 using Bezier = std::vector<Point>;
 using Buffer = std::vector<std::vector<Point>>;
 using Normals = std::vector<std::vector<Segment>>;
-using Tengents = std::vector<std::vector<Segment>>;
+using Tangents = std::vector<std::vector<Segment>>;
 
 // Point evaluate(const Bezier& curve, double t)
 // {
@@ -64,25 +65,38 @@ using Tengents = std::vector<std::vector<Segment>>;
 
 // }
 
-
 Bezier derivate(const Bezier &curve)
 {
 
-    //:SOURCE: https://pages.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/Bezier/bezier-der.html
-    //:COMMENT: pas sur que ca marche ... 
+    //: SOURCE: https://pages.mtu.edu/~shene/COURSES/cs3621/NOTES/spline/Bezier/bezier-der.html
+    //: COMMENT: pas sur que ca marche ...
     Bezier C1 = curve;
     Bezier C2 = curve;
     Bezier retDeriv(curve.size() - 1);
 
-
-    for (size_t i = 1; i < curve.size() ; i++)
+    for (size_t i = 1; i < curve.size(); i++)
     {
-        retDeriv[i-1] = subPoint(curve.at(i),curve.at(i-1), curve.size() - 1);
+        retDeriv[i - 1] = subPoint(curve.at(i), curve.at(i - 1), curve.size() - 1);
     }
     return retDeriv;
 }
 
+Point computeTengent(Point deriv_time_t)
+{
+    //: SOURCE: https://pomax.github.io/bezierinfo/#derivatives
+    double m = sqrt(deriv_time_t.x * deriv_time_t.x +
+                    deriv_time_t.y * deriv_time_t.y);
+    return Point({deriv_time_t.x / m, deriv_time_t.y / m});
+}
 
+Point computeNormal(Point tengent)
+{
+    //: SOURCE: https://pomax.github.io/bezierinfo/#derivatives
+    double m = sqrt(tengent.x * tengent.x +
+                    tengent.y * tengent.y);
+
+    return Point({-tengent.y / m, tengent.x / m});
+}
 
 void writePointsVTK(const std::vector<Point> &points, const std::string &filePath)
 {
@@ -126,22 +140,22 @@ void writeLinesVTK(const std::vector<Point> &points, const std::string &filePath
 
 // void writeSemgentVTK(const std::vector<Point> &points, const std::string &filePath)
 // {
-    // std::ofstream out{filePath};
+// std::ofstream out{filePath};
 
-    // out << "# vtk DataFile Version 2.0\n";
-    // out << "CGT vtk points output\n";
-    // out << "ASCII\n";
-    // out << "DATASET POLYDATA\n";
+// out << "# vtk DataFile Version 2.0\n";
+// out << "CGT vtk points output\n";
+// out << "ASCII\n";
+// out << "DATASET POLYDATA\n";
 
-    // out << "POINTS " << points.size() << " float\n";
-    // for (const auto &point : points)
-    //     out << point.x << ' ' << point.y << " 0\n";
+// out << "POINTS " << points.size() << " float\n";
+// for (const auto &point : points)
+//     out << point.x << ' ' << point.y << " 0\n";
 
-    // out << "LINES 1 " << (points.size() + 1) << "\n";
-    // out << points.size();
-    // for (size_t i = 0, n = points.size(); i < n; ++i)
-    //     out << ' ' << i;
-    // out << "\n";
+// out << "LINES 1 " << (points.size() + 1) << "\n";
+// out << points.size();
+// for (size_t i = 0, n = points.size(); i < n; ++i)
+//     out << ' ' << i;
+// out << "\n";
 // }
 
 int binomial(int n, int k)
@@ -195,23 +209,51 @@ Point computeCasteljau(Buffer &tmp, double t)
     return tmp[n - 1][0];
 }
 
-void casteljau(const std::vector<Point> &tab, std::vector<Point> &curve, const int nb_points_on_curve)
+Buffer createBuffer(const Bezier &tab)
 {
-    //: SOURCE: https://fr.wikipedia.org/wiki/Algorithme_de_Casteljau
-
     Buffer tmp(tab.size());
-    Bezier ret;
-
     tmp[0] = tab;
 
     for (int i = 1; i < tab.size(); i++)
     {
         tmp[i].resize(tab.size() - i);
     }
+    return tmp;
+}
+
+void casteljau(const Bezier &tab, std::vector<Point> &curve, const int nb_points_on_curve)
+{
+    //: SOURCE: https://fr.wikipedia.org/wiki/Algorithme_de_Casteljau
+
+    Buffer bufferCurve = createBuffer(tab);
 
     for (int i = 0; i <= nb_points_on_curve; i++)
     {
-        curve.push_back(computeCasteljau(tmp, static_cast<double>(i) / static_cast<double>(nb_points_on_curve) * 1.));
+        curve.push_back(computeCasteljau(bufferCurve, static_cast<double>(i) / static_cast<double>(nb_points_on_curve) * 1.));
+    }
+}
+
+void normalsAndTangents(const Bezier &tab, std::vector<Point> &curve, const int nb_points_on_curve,
+                        Tangents &tengents, Normals &normals)
+{
+
+    Buffer bufferCurve = createBuffer(tab);
+    Bezier deriv = derivate(tab);
+    Buffer bufferDeriv = createBuffer(deriv);
+
+
+    for (int i = 0; i <= nb_points_on_curve; i++)
+    {
+        double time = static_cast<double>(i) / static_cast<double>(nb_points_on_curve) * 1.;
+
+        Point curvePoint = computeCasteljau(bufferCurve, time);
+
+        curve.push_back(curvePoint);
+        Point deriv = computeCasteljau(bufferDeriv, time);
+        Point tangent = computeTengent(deriv);
+        Point normal = computeNormal(tangent);
+
+
     }
 }
 
@@ -266,7 +308,7 @@ int main(int, char **)
     {
         // auto timer = Timer{ "Casteljau algorithm with " + std::to_string(nb_points_on_curve) + " points compute. Time : " };
         for (int i = 0; i < 100; ++i)
-            casteljau(points, curve_castel, nb_points_on_curve);
+            casteljau(points, curve_castel, nb_points_on_curve, false);
         writeLinesVTK(curve_castel, "result/curve_castel.vtk");
     }
 
@@ -301,6 +343,13 @@ int main(int, char **)
         casteljau(derivOf, curv_deriv, nb_points_on_curve);
         writeLinesVTK(curv_deriv, "result/curv_deriv.vtk");
         writeLinesVTK(derivOf, "result/point_deriv.vtk");
+    }
+
+    {
+        // auto timer = Timer{ "Casteljau algorithm with " + std::to_string(nb_points_on_curve) + " points compute. Time : " };
+
+        casteljau(points, curve_castel, nb_points_on_curve);
+        writeLinesVTK(curve_castel, "result/curve_castel.vtk");
     }
 
     writeLinesVTK(points, "result/lines.vtk");
