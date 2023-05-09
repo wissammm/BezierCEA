13493a4b7e682bee7a74629474dc909269764630
wissammm <wissam.boussella@cea.fr>
2023-05-09 09:12:25 +0200

    j'enleve tout le cracra

--- build/tests/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/WBO/BezierCEA/tests
-# Build directory: C:/WBO/BezierCEA/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(runTests "runTests")
-set_tests_properties(runTests PROPERTIES  _BACKTRACE_TRIPLES "C:/WBO/BezierCEA/tests/CMakeLists.txt;18;add_test;C:/WBO/BezierCEA/tests/CMakeLists.txt;0;")
