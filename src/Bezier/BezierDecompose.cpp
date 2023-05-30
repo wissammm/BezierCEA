3b79a0c084df6d0fa98917dd2b6e7ebb47f25e6e
Boussella Wissam <wissamboussella@gmail.com>
2023-05-30 10:25:48 +0200

    Jamais point on curve marche mais on essaye quand meme

--- src/Bezier/BezierDecompose.cpp
+++ src/Bezier/BezierDecompose.cpp
@@ -1,5 +1,8 @@
 #include "BezierDecompose.h"
 
+
+
+
 std::array<Bezier, 2> decompose(const Bezier& curve, double t) {
 
     auto buffer = createBuffer(curve.degree());
@@ -14,21 +17,7 @@ std::array<Bezier, 2> decompose(const Bezier& curve, double t) {
         part1.controlPoint[i] = buffer[i][0];
         part2.controlPoint[curve.nbControlPoint() - i - 1] = buffer[i][curve.nbControlPoint() - i - 1];
     }
-    // if (curve.roots.size() > 0) {
-    //     for (size_t i = 0; i < curve.roots.size(); ++i) {
-    //         if (curve.roots[i].time > t) {
-    //             part2.roots.push_back(curve.roots[i]);
-    //         } else {
-    //             part1.roots.push_back(curve.roots[i]);
-    //         }
-    //     }
-    // }
 
-    // if (curve.lut.size() > 0) {
-    //     auto n    = curve.lut.size() * t;
-    //     part1.lut = std::vector(curve.lut.begin(), curve.lut.begin() + n);
-    //     part1.lut = std::vector(curve.lut.begin() + n, curve.lut.end());
-    // }
     std::array<Bezier, 2> ret = {part1, part2};
     return ret;
 }
--- src/Bezier/PointOnCurve.cpp
+++ src/Bezier/PointOnCurve.cpp
@@ -10,42 +10,51 @@ CoordTime getNearestPointOnCurve(Bezier bez, Coord p, size_t nbIter) {
 
     auto                  minDist = std::numeric_limits<double>::max();
     BezierWithInitialTime part    = BezierWithInitialTime({bez, 0, 1});
-    // size_t                lutMinDist;
-    // std::vector<double>   tmpDist(lut.size());
-    // for (size_t n = 0; n < nbIter; ++n) {
-    //     auto lut = computeLUT(part.bez, bez.nbControlPoint());
-    //     for (size_t i = 0; i < lut.size(); ++i) {
-    //         tmpDist[i] = distance(lut[i].coord, p);
-    //         if (tmpDist[i] < minDist) {
-    //             lutMinDist = i;
-    //             minDist    = tmpDist[i];
-    //         }
-    //     }
-    //     if (lutMinDist == 0) {
-    //         part = BezierWithInitialTime(        //
-    //             {decompose(bez, lut[1].time)[0], //
-    //              part.tBegin,                    //
-    //              part.tBegin + lut[1].time * (part.tEnd - part.tBegin)});
-    //     } else if (lutMinDist == bez.nbControlPoint() - 1) {
-    //         part = BezierWithInitialTime(                                                      //
-    //             {decompose(bez, lut[bez.nbControlPoint() - 2].time)[1],                        //
-    //              part.tBegin + lut[bez.nbControlPoint() - 2].time * (part.tEnd - part.tBegin), //
-    //              part.tEnd});
-    //     } else {
-    //         if (tmpDist[lutMinDist - 1] < tmpDist[lutMinDist + 1]) {
-    //             auto tmpEnd = part.tBegin + lut[lutMinDist].time * (part.tEnd - part.tBegin);
-    //             part        = BezierWithInitialTime(                                            //
-    //                 {decompose(bez, lut[lutMinDist - 1].time)[0],                        //
-    //                         part.tBegin + lut[lutMinDist - 1].time * (part.tEnd - part.tBegin), //
-    //                         part.tEnd});
-    //             part        = BezierWithInitialTime(                                            //
-    //                 {decompose(bez, lut[1].time)[1],                                     //
-    //                         part.tBegin,                                                        //
-    //                         part.tBegin + lut[lutMinDist].time * (part.tEnd - part.tBegin)});
-    //         } else {
-                
-    //         }
-    //     }
-    // }
+
+    size_t              lutMinDist;
+    std::vector<double> tmpDist(bez.nbControlPoint());
+    for (size_t n = 0; n < nbIter; ++n) {
+        auto lut = computeLUT(part.bez, bez.nbControlPoint());
+        for (size_t i = 0; i < lut.size(); ++i) {
+            tmpDist[i] = distance(lut[i].coord, p);
+            if (tmpDist[i] < minDist) {
+                lutMinDist = i;
+                minDist    = tmpDist[i];
+            }
+        }
+        if (lutMinDist == 0) {
+            part = BezierWithInitialTime(        //
+                {decompose(bez, lut[1].time)[0], //
+                 part.tBegin,                    //
+                 part.tBegin + lut[1].time * (part.tEnd - part.tBegin)});
+        } else if (lutMinDist == bez.nbControlPoint() - 1) {
+            part = BezierWithInitialTime(                                                      //
+                {decompose(bez, lut[bez.nbControlPoint() - 2].time)[1],                        //
+                 part.tBegin + lut[bez.nbControlPoint() - 2].time * (part.tEnd - part.tBegin), //
+                 part.tEnd});
+        } else {
+            if (tmpDist[lutMinDist - 1] < tmpDist[lutMinDist + 1]) {
+                double tmpTbegin = part.tBegin;
+                part             = BezierWithInitialTime(                                            //
+                    {decompose(bez, lut[lutMinDist - 1].time)[1],                        //
+                                 part.tBegin + lut[lutMinDist - 1].time * (part.tEnd - part.tBegin), //
+                                 part.tEnd});
+                part             = BezierWithInitialTime(                                            //
+                    {decompose(bez, lut[lutMinDist].time * (1 - tmpTbegin))[0],          //
+                                 part.tBegin,                                                        //
+                                 part.tBegin + (lut[lutMinDist].time * (1 - tmpTbegin)) * (part.tEnd - part.tBegin)});
+            } else {
+                double tmpTbegin = part.tBegin;
+                part             = BezierWithInitialTime(                                        //
+                    {decompose(bez, lut[lutMinDist].time)[1],                        //
+                                 part.tBegin + lut[lutMinDist].time * (part.tEnd - part.tBegin), //
+                                 part.tEnd});
+                part             = BezierWithInitialTime(                                        //
+                    {decompose(bez, lut[lutMinDist + 1].time * (1 - tmpTbegin))[0],  //
+                                 part.tBegin,                                                    //
+                                 part.tBegin + (lut[lutMinDist + 1].time * (1 - tmpTbegin)) * (part.tEnd - part.tBegin)});
+            }
+        }
+    }
     return CoordTime();
 }
